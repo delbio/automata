3220b5967f67da991f34553270edb95cbb7bd56b
Fabio Del Bene <delbio87@gmail.com>
2017-06-03 21:20:09 +0200

    test addAction_action-have-another-origin-state_throwError

--- src/state.class.jest.test.ts
+++ src/state.class.jest.test.ts
@@ -12,15 +12,19 @@ describe('State', () => {
         const state = new State();
         expect(state.getName()).toBe('State');
     });
-    it('emptyActionMap', () => {
+    it('constructor_empty_next_actions', () => {
         const state = new State();
-        expect(state.getNextActions().length).toBe(0);
+        expect(state.getNextInputs().length).toBe(0);
+    });
+    it('constructor_empty_next_inputs', () => {
+        const state = new State();
         expect(state.getNextInputs().length).toBe(0);
     });
     it('addAction_action-have-another-origin-state_throwError', () => {
         const state = new State();
         const state1 = new State1();
         const action = new Action(state1);
-        state.addAction(action);
+        expect(() =>{ state.addAction(action); }).toThrow(Error);
+        expect(() =>{ state.addAction(action); }).toThrow("L'origine della azione deve essere questo stato");
     });
 });
