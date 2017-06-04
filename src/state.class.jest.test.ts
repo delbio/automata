0d95f3a0f2f1fe22d4ef76e5b16e913ec5a79224
Fabio Del Bene <delbio87@gmail.com>
2017-06-04 11:06:24 +0200

    added some test to State Class

--- src/state.class.jest.test.ts
+++ src/state.class.jest.test.ts
@@ -2,23 +2,32 @@ import { State } from './state.class';
 import { Action } from './action.class';
 
 class State1 extends State {}
+class Action1 extends Action {}
 
 describe('State', () => {
     it('toString', () => {
         const state = new State();
         expect(state.toString()).toBe('State[class=State]');
     });
-    it('getName', () => {
+    it('getName_return-class-name', () => {
         const state = new State();
         expect(state.getName()).toBe('State');
     });
-    it('constructor_empty_next_actions', () => {
+    it('constructor_empty_nextActions', () => {
         const state = new State();
         expect(state.getNextInputs().length).toBe(0);
     });
-    it('constructor_empty_next_inputs', () => {
+    it('constructor_empty_nextInputs', () => {
         const state = new State();
         expect(state.getNextInputs().length).toBe(0);
+    });
+    it('addAction_action-have-same-origin-state_correct-add', () => {
+        const state = new State();
+        const action = new Action(state);
+        state.addAction(action);
+        const nextActions = state.getNextActions();
+        expect(nextActions.length).toBe(1);
+        expect(nextActions[0]).toBe(action);
     });
     it('addAction_action-have-another-origin-state_throwError', () => {
         const state = new State();
@@ -26,5 +35,15 @@ describe('State', () => {
         const action = new Action(state1);
         expect(() =>{ state.addAction(action); }).toThrow(Error);
         expect(() =>{ state.addAction(action); }).toThrow("L'origine della azione deve essere questo stato");
+    });
+    it('addAction_state-already-has-action-with-same-name_throwError', () => {
+        const state = new State();
+        const action = new Action(state);
+        state.addAction(action);
+        expect(() =>{ state.addAction(action); }).toThrow(Error);
+        expect(() =>{ state.addAction(action); }).toThrow("cannot add action Action to State[class=State]: state already has an action with the same name");
+    });
+    it('addAction_add-different-action_correctly-added', () => {
+        
     });
 });
