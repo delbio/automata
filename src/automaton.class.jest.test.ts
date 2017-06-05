ef11645886ca568f5126b20ca3e09b4c8acc9b11
Fabio Del Bene <delbio87@gmail.com>
2017-06-05 23:43:45 +0200

    ignored coverage report

---
+++ src/automaton.class.jest.test.ts
@@ -0,0 +1,10 @@
+import { Action } from './action.class';
+import { State } from './state.class';
+import { Automaton } from './automaton.class';
+
+class Action1 extends Action {}
+class State1 extends State {}
+
+describe('Automaton', () => {
+
+});
