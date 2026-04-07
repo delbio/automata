cd10675a36a1ad55218bee71658bafd81d623eae
Fabio Del Bene <delbio87@gmail.com>
2026-04-07 09:57:38 +0200

    fix remove ts code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import { ActionInterface } from "./action.interface.js";
 import { Action } from "./action.class.js";
-import { StateInterface } from "./state.interface.js";
 import { State } from "./state.class.js";
 import { Automaton } from "./automaton.class.js";
-import { AutomatonInterface } from "./automaton.interface.js";
 export {
   Action,
-  ActionInterface,
   Automaton,
-  AutomatonInterface,
   State,
-  StateInterface
 };
